3a6b2c635fce8972f1d92f0ffbc2ce411e8a604e
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-20 11:06:14 +0300

    Initial Commit

---
+++ Stretching Picture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stretching Picture
+//
+//  Created by Владимир on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
